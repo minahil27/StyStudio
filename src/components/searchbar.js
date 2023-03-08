dc300da7f9a288420cf8c466edbea0920085d4f9
wasla <wasla.shafique@mlsense.ai>
2023-03-09 02:12:12 +0500

    Search Animation addded

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,24 +1,46 @@
 //import React, { useState } from 'react';
 import { IconContext } from 'react-icons';
 import {FaSearch} from 'react-icons/fa'
+import React, { useState ,useEffect, useRef} from "react";
 
 export function SearchBar(){
-//    const [searchItem,setSearchItem] = useState('');
+
+    const [isClicked,setIsClicked] = useState(false);
+    const [isbuttonClicked, setIsButtonClicked] = useState(false);
+    const searchRef = useRef(null);
+
+    useEffect(() => {
+        if (isClicked) {
+          searchRef.current.focus();
+        }
+      }, [isClicked]);
+
+    //handler fuction
+    const handleClick = () =>{
+        setIsButtonClicked(!isbuttonClicked);
+        setIsClicked(!isClicked);
+    }
+    
+
+   
 
 
     return (
         <div className="listing-text">
             <p>Find best salons in your area</p> 
-            <div className='search-icon'>
+
+
+            <div className='search-container'>
 
              
-        <IconContext.Provider  value = {{ color : 'white', size : '30px', }}>
+        <IconContext.Provider  value = {{ color : 'white', size : '30px', backgroundColor: '#07794D'}}>
 
-        <button className='search-btn'>< FaSearch/></button>
+        <button className={`search-btn ${isClicked ? "" : "active"}`} onClick = {handleClick}>< FaSearch/></button>
         </IconContext.Provider>
-        <input className='searchBar'
+        <input className={`search-bar ${isClicked ? "active" : ""}`}
             type="text"
             placeholder="Search"
+            ref = {searchRef}
             //value={searchTerm}
            // onChange={handleSearchInputChange}
           />
