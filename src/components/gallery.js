2caeaa3b1870444d306edc9ea094add65aca69f7
wasla <wasla.shafique@mlsense.ai>
2023-03-14 03:42:16 +0500

    added background radient

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -3,9 +3,10 @@ import content from '../content';
 import { SearchBar } from './searchbar';
 export function Gallery(props){
  
-    return( <>
-      
-      <div className="gallery-text"> <h1> Gallery </h1> </div>
+    return(<>
+    
+    <div className='bg-image'>
+        <div className="gallery-text"> <h1> Gallery </h1> </div>
       <SearchBar/>
         {/*salon listing starts here*/}
         <div className='listing'>
@@ -17,6 +18,6 @@ export function Gallery(props){
         </div> {/*salon listing starts here*/}
         <div className='salon-reg'><h4 className='salon-reg-text'>Want to become the best salon in your area?</h4> <button className='btn'>Register Now!!</button></div>
         <div className='salon-appointment'><h4 className='salon-reg-text'>Book your appointment today in one of the best salons </h4> <button className='btn'> Book An Appointment</button></div>
-        </>
+        </div></> 
     )
 }
--- src/components/salons.js
+++ src/components/salons.js
@@ -5,10 +5,10 @@ export function Salons(props){
         <div key = {props.id} className="salon-card">
             <img src = {props.image} alt="salon img" className="salon-image"></img>
         
-        <div className="salon-content">
+        {/*<div className="salon-content">*/}
             <h3 className="salon-name"> {props.name}</h3>
-            <h5 className="salon-description"> {props.description}</h5>
-        </div>
+            <p className="salon-description"> {props.description}</p>
+        {/*</div>*/}
         </div>
         </div>
         </>
